89e0e44b5d8723aa3f3062a7a9b8951b496fadc7
Dan Moore <onedle.rocks@danielmoore.xyz>
2022-02-28 20:59:24 -0500

    Add a humble description and better attribution info

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,55 +8,56 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 2 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
-      </p>
+      <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+              Guess the word in 2 tries. After each guess, the color of the tiles will
+              change to show how close your guess was to the word.
+          </p>
 
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="A"
-          status="correct"
-        />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter A is in the word and in the correct spot.
-      </p>
+          <div className="flex justify-center mb-1 mt-4">
+              <Cell
+                  isRevealing={true}
+                  isCompleted={true}
+                  value="A"
+                  status="correct"
+              />
+          </div>
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+              The letter A is in the word and in the correct spot.
+          </p>
 
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="I"
-          status="present"
-        />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter I is in the word but in the wrong spot.
-        This is very unlikely to happen to you while playing Onedle.
-      </p>
+          <div className="flex justify-center mb-1 mt-4">
+              <Cell
+                  isRevealing={true}
+                  isCompleted={true}
+                  value="I"
+                  status="present"
+              />
+          </div>
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+              The letter I is in the word but in the wrong spot.
+              This is very unlikely to happen to you while playing Onedle.
+          </p>
 
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell isRevealing={true} isCompleted={true} value="A" status="absent" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter A is not in the word in any spot.
-      </p>
+          <div className="flex justify-center mb-1 mt-4">
+              <Cell isRevealing={true} isCompleted={true} value="A" status="absent" />
+          </div>
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+              The letter A is not in the word in any spot.
+          </p>
 
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love.
-        It is based on react-wordle  -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
-      </p>
-    </BaseModal>
+          <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+              Onedle is the perfected form of Wordle based on
+              <a href="https://github.com/cwackerfuss/react-wordle">
+                  Chase Wackerfuss's React clone
+              </a>.  -{' '}
+              <a
+                  href="https://github.com/drmrd/onedle"
+                  className="underline font-bold"
+              >
+                  Check out the code here!
+              </a>{' '}
+          </p>
+      </BaseModal>
   )
 }
